db2acf8942e3988e0ef3b502479fa87e4f5392e2
vitorshaft <vtrshaft@gmail.com>
2020-04-24 23:59:11 -0300

    implementacao da leitura/transmissao IR

--- kabuto2_IR_REC/kabuto2_IR_REC.ino
+++ kabuto2_IR_REC/kabuto2_IR_REC.ino
@@ -1,8 +1,20 @@
-const int pinoFototransistor = A2; //PINO ANALÓGICO UTILIZADO PELO FOTOTRANSISTOR
-               
+/*Projeto Kabuto 2
+Vitor Domingues
+https://github.com/vitorshaft/kabuto
+*/
+//IR transmissor: tem 2 pinos (transparente)
+//IR receptor: tem 3 pinos (escuro)
+#include <SoftwareSerial.h>
+#define TX 13
+#define RX 8 //PINO ANALÓGICO UTILIZADO PELO FOTOTRANSISTOR
+
+
+
 void setup(){
   Serial.begin(9600); //INICIALIZAÇÃO DA SERIAL
-   pinMode(pinoFototransistor, INPUT); //DEFINE O PINO COMO ENTRADA
+  pinMode(pinoFototransistor, INPUT); //DEFINE O PINO COMO ENTRADA
+  pinMode(TX, OUTPUT);
+  digitalWrite(TX, HIGH); //transmissor ligado direto
 }
 
 void loop(){
